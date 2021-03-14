09a65a70f185d6ce1476eabf25bdb9ea2fa13a26
Noah Holt <70286103+TidalTurtles@users.noreply.github.com>
2021-03-14 11:05:49 -0600

    Getting started on my homework
    created most of the methods i think i need and started with the printed objects

---
+++ HoltNoahAssignment7.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+public class HoltNoahAssignment7 {
+
+	public static void main(String[] args) {
+		/*Noah Holt
+		 * CS1150 Principles of computer science
+		 * m/w section
+		 * Due 17 mar
+		 * Assignment 7
+		 * This assignment is to create a area calculator to get us 
+		 * better oriented with using different methods and so we will
+		 * Calculate the area of different shapes with different dimensions
+		 * based on user inputs
+		 */
+		
+		Scanner user = new Scanner(System.in);
+		
+		System.out.println("*********************************");
+		System.out.println(" Welcome to the Geometry Program");
+		System.out.println("*********************************");
+		System.out.println(" ");
+		System.out.println("Compute Area (Y/N):");
+		
+		int userInput
+		
+		user.close();
+	}
+	
+	public static int getShape(Scanner userShape) {
+		
+		
+	}
+	
+	public static double circleArea(double radius) {
+		
+		
+	}
+	
+	public static double pentagonArea(double length) {
+		
+	}
+	
+	public static double hexagonArea(double side) {
+		
+		
+	}
+	
+	public static boolean keepGoing(Scanner userInput) {
+		
+	}
+
+}
